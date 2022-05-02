3d49c56b39895c82b4201a6b080121ed76fb90f8
Willen Leiros <willenleiros@jfrn.jus.br>
2022-05-01 22:46:47 -0300

    feat: adicionando config locais e provendo RestTemplate

--- src/main/java/com/example/sbootatomcongresso/controller/EventoController.java
+++ src/main/java/com/example/sbootatomcongresso/controller/EventoController.java
@@ -1,10 +1,22 @@
 package com.example.sbootatomcongresso.controller;
 
 import com.example.sbootatomcongresso.domain.Evento;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,23 +26,52 @@ import java.util.List;
 public class EventoController {
 
     private List<Evento> eventos;
+    private RestTemplate restTemplate;
+    Logger logger = LoggerFactory.getLogger(EventoController.class);
 
-    public EventoController(){
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    public EventoController(RestTemplate restTemplate){
+        this.restTemplate = restTemplate;
+
         this.eventos = new ArrayList<>();
+
+        Evento mesaRedonda = new Evento();
+        mesaRedonda.setCodigo("MR01");
+        mesaRedonda.setNome("Mesa redonda");
+        mesaRedonda.setQuantidadePessoas(10L);
+        eventos.add(mesaRedonda);
+
+        Evento palestra = new Evento();
+        palestra.setCodigo("PA01");
+        palestra.setNome("Palestra");
+        palestra.setQuantidadePessoas(3L);
+        eventos.add(palestra);
+
     }
 
     @GetMapping
     public List<Evento> listar(){
-        Evento evento = new Evento();
-        evento.setNome("Mesa redonda");
-        evento.setQuantidadePessoas(10L);
-        eventos.add(evento);
+        InstanceInfo instance = eurekaClient.getNextServerFromEureka("ATOMCONGRESSO", false);
+        logger.info(instance.getId());
         return eventos;
     }
 
     @PostMapping
-    public Boolean credenciar(){
-        return false;
+    public ResponseEntity credenciar(){
+        MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
+        map.add("codigo", "C20");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+        String url = "http://atomficha/api/fichas";
+
+        ResponseEntity<String> response = this.restTemplate.postForEntity(url, request, String.class);
+
+        return response;
     }
 
 }
--- src/main/java/com/example/sbootatomcongresso/domain/Evento.java
+++ src/main/java/com/example/sbootatomcongresso/domain/Evento.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class Evento {
+    private String codigo;
     private String nome;
     private Long quantidadePessoas;
 }
